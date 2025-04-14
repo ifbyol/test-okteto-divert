2e41b4a77a3916a9f64fc921ab61cc866061aab1
Nacho Fuertes <nacho@okteto.com>
2025-04-14 12:46:49 +0200

    Added code for the services
    Signed-off-by: Nacho Fuertes <nacho@okteto.com>

---
+++ serviceA/index.js
@@ -0,0 +1,68 @@
+import got from "got";
+import express from "express";
+
+const app = express();
+const oktetoDivertHeader = "baggage.okteto-divert";
+const PORT = 8080;
+
+function getDivertKey(headers) {
+  if (headers && headers[oktetoDivertHeader]) {
+    return headers[oktetoDivertHeader];
+  }
+
+  return undefined;
+}
+
+function buildHeaders(headers) {
+  // propagate the baggage headers to allow the receiving service to make runtime decisions
+  var options = { headers: {} };
+  const divertKey = getDivertKey(headers);
+  if (divertKey) {
+    options.headers["baggage.okteto-divert"] = divertKey;
+    //add other headers that you might need to propagate
+  }
+
+  return options;
+}
+
+async function callDownstreamService(req) {
+  let url = `https://divert-showcase-${process.env.OKTETO_NAMESPACE}.${process.env.OKTETO_DOMAIN}/serviceb/chain`;
+  const divertKey = getDivertKey(req.headers);
+  if (divertKey) {
+    // when diverted, route the request to the service on the diverted namespace.
+    url = `https://divert-showcase-${divertKey}.${process.env.OKTETO_DOMAIN}/serviceb/chain`;
+  }
+
+
+  // const url = `https://${req.headers.host}/serviceb/chain`;
+  const options = buildHeaders(req.headers);
+  console.log(`calling service ${url} with headers ${options.headers}`);
+
+  return await got(url, options).text();
+}
+
+app.get("/servicea", function (req, res) {
+  res.send("Service A says hello!");
+});
+
+app.get("/servicea/chain", async function (req, res) {
+  console.log("/chain request");
+
+  try {
+    const data = await callDownstreamService(req);
+    const message = `Service A says hello from ${process.env.OKTETO_NAMESPACE}! <br />`;
+    res.send(message + data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("404: Sorry! Page not found.");
+  console.log("404 error occurred:", req.url);
+});
+
+app.listen(PORT, function () {
+  console.log("Started service-A server on %d", PORT);
+});
---
+++ serviceB/index.js
@@ -0,0 +1,59 @@
+import got from "got";
+import express from "express";
+
+const app = express();
+const oktetoDivertHeader = "baggage.okteto-divert";
+const PORT = 8080;
+
+function getDivertKey(headers) {
+  if (headers && headers[oktetoDivertHeader]) {
+    return headers[oktetoDivertHeader];
+  }
+
+  return undefined;
+}
+
+function buildHeaders(headers) {
+  // propagate the baggage headers to allow the receiving service to make runtime decisions
+  var options = { headers: {} };
+  const divertKey = getDivertKey(headers);
+  if (divertKey) {
+    options.headers["baggage.okteto-divert"] = divertKey;
+    //add other headers that you might need to propagate
+  }
+
+  return options;
+}
+
+async function callDownstreamService(headers) {
+  const url = `https://${headers.host}/servicec/chain`;
+  const options = buildHeaders(headers);
+  console.log(`calling service ${url}`);
+  return await got(url, options).text();
+}
+
+app.get("/serviceb", function (req, res) {
+  res.send("Service B says hello!");
+});
+
+app.get("/serviceb/chain", async function (req, res) {
+  console.log("/chain request");
+
+  try {
+    const data = await callDownstreamService(req.headers);
+    const message = `Service B says hello from ${process.env.OKTETO_NAMESPACE}! <br />`;
+    res.send(message + data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("404: Sorry! Page not found.");
+  console.log("404 error occurred:", req.url);
+});
+
+app.listen(PORT, function () {
+  console.log("Started service-b server on %d", PORT);
+});
